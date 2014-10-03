f1e14eac6dc426d2d10de50e1c8708cdbf6b0bf6
Doug Turnbull <softwaredoug@gmail.com>
2014-10-03 13:42:03 -0400

    boilerplate angular for Brian

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontendApp
+ * @description
+ * # frontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('starter-app', ['ngSanitize',
+                          'ui.bootstrap',
+                          'o19s.splainer-search']);
